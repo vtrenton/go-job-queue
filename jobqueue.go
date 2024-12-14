c9167488a15622e23465d29ad5cb505e8a5e1646
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-12-13 18:27:19 -0600

    basic build out of job queueing program
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- jobqueue.go
+++ jobqueue.go
@@ -6,13 +6,23 @@ import (
 )
 
 type Job struct {
-	ID string,
-	Payload string,
-	Status string
+	ID      string
+	Payload string
+	Status  string
 }
 
 func main() {
+	jobQueue := make(chan Job, 10)
+	StartDispatcher(jobQueue, 3)
 
+	for i := 1; i <= 5; i++ {
+		job := Job{
+			ID:      fmt.Sprintf("job-%d", i),
+			Payload: fmt.Sprintf("Payload for job-%d", i),
+		}
+		AddJob(jobQueue, job)
+	}
+	time.Sleep(5 * time.Second)
 }
 
 // Create a queue for jobs to be stored
@@ -23,14 +33,14 @@ func AddJob(queue chan Job, job Job) {
 // Workers that spawn as go routines to manage and work on jobs.
 func Worker(queue chan Job, workerID int) {
 	for job := range queue {
-		fmt.Printf("Worker %d processing job %s", workerID, job.ID)
+		fmt.Printf("Worker %d processing job %s\n", workerID, job.ID)
 		time.Sleep(1 * time.Second)
 	}
 }
 
 // Spawn concurrent workers to execute based on the number of jobs in the queue
 func StartDispatcher(queue chan Job, numWorkers int) {
-	for i := 0; i < numWorkers; i++) {
+	for i := 0; i < numWorkers; i++ {
 		go Worker(queue, i)
 	}
 }
